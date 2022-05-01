36779c202d69fb5d42a0b033de51b76b10005f44
walidboulanouar <72222865+walidboulanouar@users.noreply.github.com>
2022-05-01 06:42:45 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://newalidbou:Walid062468@cluster0.ni6do.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.ni6do.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL)
